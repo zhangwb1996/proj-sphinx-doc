7c2d51c3aa500df9c4bea4b68fb30f406982ffba
zhangwb <804158121@qq.com>
2023-11-10 16:38:33 +0800

    google search

--- clean.bat
+++ clean.bat
@@ -4,7 +4,7 @@ rmdir doctrees /Q /S
 cd html
 @REM rm -r -force *
 @REM rmdir -r -force *
-del -r -force * /Q
+del -r -force * /Q /S
 rmdir  my-notes /Q /S
 rmdir  _static /Q /S
 rmdir  _sources /Q /S
@@ -13,3 +13,7 @@ rmdir  _images /Q /S
 type NUL > .nojekyll
 type NUL > .gitignore
 echo _sources > .gitignore
+copy ..\..\googleb7191771d1c2ee0b.html googleb7191771d1c2ee0b.html
+copy ..\..\sitemap.xml sitemap.xml
+@REM type ../../googleb7191771d1c2ee0b.html > googleb7191771d1c2ee0b.html
+@REM type ../../sitemap.xml > sitemap.xml
